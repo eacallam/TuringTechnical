0f11eac621cf41b4d6e594635962ec7a29c2a4b0
eacallam <eacallam@yahoo.com>
2017-10-16 21:02:24 -0400

    First Commit

---
+++ TuringTechnical/Models/Course.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TuringTechnical.Models
+{
+    public class Course
+    {
+        [Key]
+        public int ID { get; set; }
+        public string CourseTitle { get; set; }
+        public string CourseDescription { get; set; }
+        public int CreditHours { get; set; }
+        public int MaxClassSize { get; set; }
+        public string ClassFormat { get; set; }
+
+    }
+}
---
+++ TuringTechnical/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TuringTechnical.Models
+{
+    public class Student
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        [DisplayFormat(DataFormatString ="{mm:dd:yyyy}")]
+        public DateTime DateOfBirth { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
